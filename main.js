a6033d4001b84a0c7fd14deefa878d403ef680a4
SAD1011 <77442071+SAD1011@users.noreply.github.com>
2023-01-12 17:46:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+status = "";
+objects = [];
+
+function setup() {
+    canvas.createCanvas(380,380);
+    canvas.center();
+    video.createCapture(VIDEO);
+    video.size(380,380);
+    video.hide;
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function start(){
+    objectDetector = ml5.objectDetection('cocossd, modelLoaded');
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name = document.getElementById("object_name").value;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
